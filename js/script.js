81f5e1077881d68c54f4669b5c226b0da0d1a20e
EllisOllier <ellisollier1@gmail.com>
2024-04-05 22:53:28 +0100

    Rewrote book library

--- js/script.js
+++ js/script.js
@@ -1,90 +1,83 @@
-const dialogBox = document.getElementById("dialog-book");
+const myLibrary = [];
+const btnNewBook = document.getElementById('new-book');
+const btnSubmitForm = document.getElementById('submit')
+const dialogElement = document.querySelector('dialog');
+const formElement = document.querySelector('form');
 
-function Book(name, author, pages, isRead) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-}
-
-function showForm() {
-    document.getElementById('book-name').value = "",
-    document.getElementById('book-author').value = "", 
-    document.getElementById('book-pages').value = "", 
-    document.getElementById('book-read').checked = false
-    dialogBox.show();
-}
-
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let book = new Book(
-    document.getElementById('book-name').value,
-    document.getElementById('book-author').value, 
-    document.getElementById('book-pages').value, 
-    document.getElementById('book-read').checked
-    )
-    
-    let bookCard = document.createElement("div");
-    let libraryContianer = document.getElementById('library-container');
-    let bookTitle = document.createElement('h3');
-    let authorTitle = document.createElement('h4');
-    let bookPages = document.createElement('p');
-    let readStatus = document.createElement('p');
-    let removeBook = document.createElement('button');
-
-    if(book.isRead){
-        let status = document.createElement('span');
-        status.textContent = "Read";
-        status.style.color = "green";
-        status.id = "read-status";
-        readStatus.textContent = ("Status: ");
-        readStatus.appendChild(status);
+btnNewBook.addEventListener('click', function() {
+    if(dialogElement.open){
+        dialogElement.open = false;
     } else {
-        let status = document.createElement('span');
-        status.textContent = "Not read"
-        status.style.color = "red"
-        status.id = "read-status";
-        readStatus.textContent = ("Status: ");
-        readStatus.appendChild(status);
+        dialogElement.open = true;
+        formElement.reset();
     }
-    bookTitle.textContent = book.name;
-    authorTitle.textContent =  ("by " + book.author);
-    bookPages.textContent = ("Pages: " + book.pages);
-    bookCard.id = "book-card";
-    removeBook.id = "remove-book";
-    removeBook.textContent = "Remove"
-    
-    bookCard.appendChild(bookTitle);
-    bookCard.appendChild(authorTitle);
-    bookCard.appendChild(bookPages);
-    bookCard.appendChild(readStatus);
-    bookCard.appendChild(removeBook);
-    libraryContianer.appendChild(bookCard);
-
-
-    dialogBox.close();
-    
-    removeBook.addEventListener('mousedown', (e) => {
-        while(bookCard.lastElementChild){
-            bookCard.removeChild(bookCard.lastElementChild);
-            bookCard.remove();
-        }
-    })
-
-    readStatus.addEventListener('mousedown', (e) => {
-        
-        if(e.target.id === 'read-status'){
-            if(book.isRead){
-                e.target.textContent = "Not read";
-                e.target.style.color = "red";
-                book.isRead = false;
-            } else {
-                e.target.textContent = "Read";
-                e.target.style.color = "green";
-                book.isRead = true;
-            }
-        }
-        
-    })
 })
+
+formElement.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let book = new Book(document.getElementById('book-title').value, 
+    document.getElementById('book-author').value,
+    document.getElementById('book-pages').value,
+    document.getElementById('book-read').checked);
+
+    addBookToLibrary(book);
+    dialogElement.open = false;
+})
+
+document.addEventListener('click', function(e){
+    if(e.target.id === 'remove-card'){
+        removeCard();
+    }
+})
+
+function Book(bookTitle, bookAuthor, bookPages, bookRead) {
+    this.title = bookTitle;
+    this.author = bookAuthor;
+    this.pages = bookPages;
+    this.read = bookRead;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayLibrary() {
+    let parent = document.getElementById('library-container');
+    for(let i = 0; i < myLibrary.length; i++){
+        let card = document.createElement('div');
+        card.className = 'card';
+        let title = document.createElement('p');
+        let author = document.createElement('p');
+        let pages = document.createElement('p');
+        let read = document.createElement('button');
+        let remove = document.createElement('button');
+        
+        title.textContent = myLibrary[i].title;
+        author.textContent = myLibrary[i].author;
+        pages.textContent = myLibrary[i].pages;
+        read.id = "read"
+        remove.textContent = "Remove"
+        remove.id = 'remove-card';
+
+        if(myLibrary[i].read === true){
+            read.style.backgroundColor = 'green';
+            read.textContent = "Read"
+        } else {
+            read.style.backgroundColor = 'red';
+            read.textContent = "Not Read"
+        }
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(read);
+        card.appendChild(remove);
+
+        parent.appendChild(card);
+    }
+}
+
+function removeCard() {
+    let button = document.getElementById('remove-card');
+    button.parentNode.remove();
+}
